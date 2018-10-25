fca2f0097329e5ccc31474ed8d5f2818893c5149
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-10-25 09:34:05 -0300

    First commit

---
+++ app/model/produto.model.js
@@ -0,0 +1,21 @@
+module.exports = (connection, Sequelize) => {
+    const produto = connection.define('produto', {
+        codigo: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        descricao:{
+            type: Sequelize.STRING(200)
+        },
+        valor: {
+            type:  Sequelize.DECIMAL(10,2)
+            },
+        situacao: {
+            type: Sequelize.STRING(1)
+        }
+        
+    })
+
+    return produto
+}
